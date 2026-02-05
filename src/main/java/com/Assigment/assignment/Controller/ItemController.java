a15ce98a8dffb91dd97ba0d7e70aee2de6b7766d
AditiShelar <shelaraditi749@gmail.com>
2026-02-05 13:59:13 +0530

    Initial commit

---
+++ src/main/java/com/Assigment/assignment/Controller/ItemController.java
@@ -0,0 +1,34 @@
+package com.Assigment.assignment.Controller;
+
+
+
+import com.Assigment.assignment.Entity.Item;
+import com.Assigment.assignment.Repository.ItemRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemRepository repository;
+
+    public ItemController(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    // Add new item
+    @PostMapping
+    public ResponseEntity<Item> addItem(@Valid @RequestBody Item item) {
+        return ResponseEntity.ok(repository.save(item));
+    }
+
+    // Get item by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItem(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
